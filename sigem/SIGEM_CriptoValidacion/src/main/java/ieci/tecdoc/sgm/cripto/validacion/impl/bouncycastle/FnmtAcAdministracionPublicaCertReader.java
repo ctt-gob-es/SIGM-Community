b63b0fae8ef68452c87e60f9528135159421da41
RubenO <ruben.ortiz@unavarra.es>
2018-05-22 09:59:59 +0200

    Cambiar obtencion informacion certificados FNMT para adaptarse cambio posiciones informacion de los nuevos certificados

--- sigem/SIGEM_CriptoValidacion/src/main/java/ieci/tecdoc/sgm/cripto/validacion/impl/bouncycastle/FnmtAcAdministracionPublicaCertReader.java
+++ sigem/SIGEM_CriptoValidacion/src/main/java/ieci/tecdoc/sgm/cripto/validacion/impl/bouncycastle/FnmtAcAdministracionPublicaCertReader.java
@@ -34,6 +34,118 @@ public class FnmtAcAdministracionPublicaCertReader implements IReaderCert {
 	    
 	    String [] datosDni = asunto.split(SEPARADOR_CAMPOS_SUBJECT_DN);	 
 	    
+	    //[Ruben #578546] dejamos en getInfo_antiguo la recuperación de datos de los certificados como se hacia antes buscando en posiciones fijas
+	    //Incluimos aquí nueva forma de búsqueda con bucle for e ifs para buscar los campos independiente de posiciones fijas para no tener problemas con
+	    // cambios en los aasuntos de los certificados al renovarlos (ha pasado con el de Sello de Organo que trae un nuevo campo).
+	    String [] nombreEntidad={""};
+		String [] ou_nombreEntidad={""};
+		
+		for (int i=0;i<datosDni.length;i++){
+			//Nombre y dni
+	    	if (datosDni[i].indexOf(CN)>-1){
+	    	    String nombreDni = datosDni[i];
+	    		String [] vNombreDni = nombreDni.split(SEPARADOR);
+	    		nombre = vNombreDni[0].split(CN)[1];
+	    		
+	    		try{
+	    			nif = vNombreDni[1];
+	    		}
+	    		catch(Exception e){
+	    			nif ="";
+	    		}	
+	    	}
+	    	//Nombre sin apellidos
+	    	if (datosDni[i].indexOf(GIVENNAME)>-1){
+	    		String nombre_sinapellidos_temp = datosDni[i];
+	    		String [] vNombreSinApellidos = nombre_sinapellidos_temp.split(GIVENNAME);
+	    		
+	    		try{
+	    			nombre_sinapellidos = vNombreSinApellidos[1];
+	    		}
+	    		catch(Exception e){
+	    			nombre_sinapellidos = nombre;
+	    		}
+	    	}
+	    	//En el asunto el serial number viene con el dni, lo asigno como cif
+	    	if (datosDni[i].indexOf(SERIALNUMBER)>-1){
+	    		String [] cifSeriarNumber;
+	    		
+	    		cifSeriarNumber= datosDni[i].split(SERIALNUMBER);		
+	    		cif = cifSeriarNumber[1];					
+	    	}
+	    	if (datosDni[i].indexOf(O)>-1){
+	    		nombreEntidad = datosDni[i].split(O);
+	    	}
+	    	if (datosDni[i].indexOf(OU)>-1){
+	    		ou_nombreEntidad  = datosDni[i].split(OU);		
+	    	}
+	    }
+		nombre_entidad = nombreEntidad[1].concat(" - ").concat(ou_nombreEntidad[1]);
+		
+		if(nif.equals("")){
+			nif = cif;
+		}
+		
+		if(nombre_entidad.equals("")){
+			nombre_entidad = nombre;
+		}
+    	
+		//Numero de serie del certificado
+    	numeroSerie = cert.getSerialNumber().toString();
+
+        InfoCertificado infoCertificado = new InfoCertificado();
+        
+        if (cif!=null){   
+        	cif = cif.replace("\"", "");
+        	cif = cif.replace("DNI", "");
+        }
+        
+        if (issuer!=null)
+        	issuer = issuer.replace("\"", "");
+        
+        if (nombre!=null)
+        	nombre = nombre.replace("\"", "");
+        
+        if (nombre_sinapellidos!=null)
+        	nombre_sinapellidos = nombre_sinapellidos.replace("\"", "");
+        
+        if (nif!=null){
+        	nif = nif.replace("\"", "");
+        	nif = nif.replace("DNI", "");
+        }
+        
+        if (numeroSerie!=null)
+        	numeroSerie = numeroSerie.replace("\"", "");
+        
+        if (asunto!=null)
+        	asunto = asunto.replace("\"", "");          
+        
+        infoCertificado.setCif(cif);
+	    infoCertificado.setIssuer(issuer);
+	    infoCertificado.setName(nombre);
+	    infoCertificado.setCorporateName(nombre_entidad);
+	    infoCertificado.setFirstname(nombre_sinapellidos);
+	    infoCertificado.setNif(nif);
+	    infoCertificado.setSerialNumber(numeroSerie);
+	    infoCertificado.setSubject(asunto);
+	    
+	    return infoCertificado;
+	}
+	
+	public InfoCertificado getInfo_antiguo(X509Certificate cert) {
+		
+		String nombre="";
+		String nombre_sinapellidos="";		
+	    String nif="";
+	    String cif="";
+	    String numeroSerie="";
+	    String nombre_entidad="";
+	    	    
+	    String issuer = cert.getIssuerDN().toString();
+	    String asunto = cert.getSubjectDN().toString();
+	    
+	    String [] datosDni = asunto.split(SEPARADOR_CAMPOS_SUBJECT_DN);	 
+	    
 	    //Nombre y dni
 	    String nombreDni = datosDni[0];
 		String [] vNombreDni = nombreDni.split(SEPARADOR);
