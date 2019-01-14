aa87e4f895ebe85b2b6c9a3648827789c5d71868
Manuel Quesada Elvira <manuel_quesada@dipucr.es>
2019-01-14 11:04:35 +0100

    [Dipucr-Manu] * Actualización de Órganos Colegiados

---
+++ sigem/SIGEM_BD/PostgreSQL/actualizacionesQuijote5.0/tramitador/04-OrganosColegiados.sql
@@ -0,0 +1,116 @@
+insert into spac_ct_entidades (id, tipo, nombre, campo_pk, sec_pk, definicion, fecha)
+values 
+(nextval('spac_sq_id_ctentidades'),'3','SECR_PLANTILLAS_SESION_ORD','ID','SPAC_SQ_63893350','<entity version=''1.00''><editable>S</editable><dropable>S</dropable><status>0</status><fields><field id=''1''><physicalName>id</physicalName><descripcion><![CDATA[Identificador inequívoco del valor]]></descripcion><type>3</type><nullable>N</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''2''><physicalName>valor</physicalName><descripcion><![CDATA[Valor que se almacena en el campo validado]]></descripcion><type>0</type><size>10</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''3''><physicalName>sustituto</physicalName><descripcion><![CDATA[Texto que se muestra en la lista de selección]]></descripcion><type>0</type><size>500</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''4''><physicalName>vigente</physicalName><descripcion><![CDATA[Indica si el valor está en vigencia para ser seleccionado]]></descripcion><type>2</type><size>1</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''5''><physicalName>orden</physicalName><descripcion><![CDATA[Indica el orden del valor]]></descripcion><type>2</type><size>10</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field></fields></entity>','2016-04-27 08:06:08');
+
+insert into spac_ct_entidades (id, tipo, nombre, campo_pk, sec_pk, definicion, fecha)
+values 
+(nextval('spac_sq_id_ctentidades'),'3','SECR_PLANTILLAS_SESION_EXTR','ID','SPAC_SQ_1980985416','<entity version=''1.00''><editable>S</editable><dropable>S</dropable><status>0</status><fields><field id=''1''><physicalName>id</physicalName><descripcion><![CDATA[Identificador inequívoco del valor]]></descripcion><type>3</type><nullable>N</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''2''><physicalName>valor</physicalName><descripcion><![CDATA[Valor que se almacena en el campo validado]]></descripcion><type>0</type><size>10</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''3''><physicalName>sustituto</physicalName><descripcion><![CDATA[Texto que se muestra en la lista de selección]]></descripcion><type>0</type><size>500</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''4''><physicalName>vigente</physicalName><descripcion><![CDATA[Indica si el valor está en vigencia para ser seleccionado]]></descripcion><type>2</type><size>1</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''5''><physicalName>orden</physicalName><descripcion><![CDATA[Indica el orden del valor]]></descripcion><type>2</type><size>10</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field></fields></entity>','2016-04-27 08:06:35');
+
+insert into spac_ct_entidades (id, tipo, nombre, campo_pk, sec_pk, definicion, fecha)
+values 
+(nextval('spac_sq_id_ctentidades'),'3','SECR_PLANTILLAS_SESION_EXUR','ID','SPAC_SQ_1980985385','<entity version=''1.00''><editable>S</editable><dropable>S</dropable><status>0</status><fields><field id=''1''><physicalName>id</physicalName><descripcion><![CDATA[Identificador inequívoco del valor]]></descripcion><type>3</type><nullable>N</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''2''><physicalName>valor</physicalName><descripcion><![CDATA[Valor que se almacena en el campo validado]]></descripcion><type>0</type><size>10</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''3''><physicalName>sustituto</physicalName><descripcion><![CDATA[Texto que se muestra en la lista de selección]]></descripcion><type>0</type><size>500</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''4''><physicalName>vigente</physicalName><descripcion><![CDATA[Indica si el valor está en vigencia para ser seleccionado]]></descripcion><type>2</type><size>1</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''5''><physicalName>orden</physicalName><descripcion><![CDATA[Indica el orden del valor]]></descripcion><type>2</type><size>10</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field></fields></entity>','2016-04-27 08:06:56');
+
+insert into spac_ct_entidades (id, tipo, nombre, campo_pk, sec_pk, definicion, fecha)
+values 
+(nextval('spac_sq_id_ctentidades'),'3','SECR_PLANTILLAS_SESION_CONS','ID','SPAC_SQ_1981053832','<entity version=''1.00''><editable>S</editable><dropable>S</dropable><status>0</status><fields><field id=''1''><physicalName>id</physicalName><descripcion><![CDATA[Identificador inequívoco del valor]]></descripcion><type>3</type><nullable>N</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''2''><physicalName>valor</physicalName><descripcion><![CDATA[Valor que se almacena en el campo validado]]></descripcion><type>0</type><size>10</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''3''><physicalName>sustituto</physicalName><descripcion><![CDATA[Texto que se muestra en la lista de selección]]></descripcion><type>0</type><size>500</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''4''><physicalName>vigente</physicalName><descripcion><![CDATA[Indica si el valor está en vigencia para ser seleccionado]]></descripcion><type>2</type><size>1</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field><field id=''5''><physicalName>orden</physicalName><descripcion><![CDATA[Indica el orden del valor]]></descripcion><type>2</type><size>10</size><nullable>S</nullable><documentarySearch>N</documentarySearch><multivalue>N</multivalue></field></fields></entity>','2016-04-27 08:07:16');
+
+
+-- SECR_PLANTILLAS_SESION_ORD
+select nextval('spac_sq_id_entidadesresources'), id, 'es', 'SECR_PLANTILLAS_SESION_ORD', 'Plantillas para COMI sesión ordinaria' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_ORD';
+select nextval('spac_sq_id_entidadesresources'), id, 'gl', 'SECR_PLANTILLAS_SESION_ORD', 'Plantillas para COMI sesión ordinaria' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_ORD';
+select nextval('spac_sq_id_entidadesresources'), id, 'eu', 'SECR_PLANTILLAS_SESION_ORD', 'Plantillas para COMI sesión ordinaria' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_ORD';
+select nextval('spac_sq_id_entidadesresources'), id, 'ca', 'SECR_PLANTILLAS_SESION_ORD', 'Plantillas para COMI sesión ordinaria' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_ORD';
+
+-- SECR_PLANTILLAS_SESION_EXTR
+select nextval('spac_sq_id_entidadesresources'), id, 'es', 'SECR_PLANTILLAS_SESION_EXTR', 'Plantillas para COMI sesión extraordinaria' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_EXTR';
+select nextval('spac_sq_id_entidadesresources'), id, 'gl', 'SECR_PLANTILLAS_SESION_EXTR', 'Plantillas para COMI sesión extraordinaria' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_EXTR';
+select nextval('spac_sq_id_entidadesresources'), id, 'eu', 'SECR_PLANTILLAS_SESION_EXTR', 'Plantillas para COMI sesión extraordinaria' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_EXTR';
+select nextval('spac_sq_id_entidadesresources'), id, 'ca', 'SECR_PLANTILLAS_SESION_EXTR', 'Plantillas para COMI sesión extraordinaria' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_EXTR';
+
+-- SECR_PLANTILLAS_SESION_EXUR
+select nextval('spac_sq_id_entidadesresources'), id, 'es', 'SECR_PLANTILLAS_SESION_EXUR', 'Plantillas para COMI sesión extraordinaria y urgente' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_EXUR';
+select nextval('spac_sq_id_entidadesresources'), id, 'gl', 'SECR_PLANTILLAS_SESION_EXUR', 'Plantillas para COMI sesión extraordinaria y urgente' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_EXUR';
+select nextval('spac_sq_id_entidadesresources'), id, 'eu', 'SECR_PLANTILLAS_SESION_EXUR', 'Plantillas para COMI sesión extraordinaria y urgente' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_EXUR';
+select nextval('spac_sq_id_entidadesresources'), id, 'ca', 'SECR_PLANTILLAS_SESION_EXUR', 'Plantillas para COMI sesión extraordinaria y urgente' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_EXUR';
+
+-- SECR_PLANTILLAS_SESION_CONS
+select nextval('spac_sq_id_entidadesresources'), id, 'es', 'SECR_PLANTILLAS_SESION_CONS', 'Plantillas para COMI sesión constitutiva' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_CONS';
+select nextval('spac_sq_id_entidadesresources'), id, 'gl', 'SECR_PLANTILLAS_SESION_CONS', 'Plantillas para COMI sesión constitutiva' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_CONS';
+select nextval('spac_sq_id_entidadesresources'), id, 'eu', 'SECR_PLANTILLAS_SESION_CONS', 'Plantillas para COMI sesión constitutiva' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_CONS';
+select nextval('spac_sq_id_entidadesresources'), id, 'ca', 'SECR_PLANTILLAS_SESION_CONS', 'Plantillas para COMI sesión constitutiva' from spac_ct_entidades where nombre ='SECR_PLANTILLAS_SESION_CONS';
+
+
+CREATE TABLE secr_plantillas_sesion_cons
+(
+  id integer NOT NULL,
+  valor character varying(10),
+  sustituto character varying(500),
+  vigente numeric(1,0),
+  orden numeric(10,0),
+  CONSTRAINT pk_1981053832 PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE spac_sq_1981053832
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+--------------------------------------------------------------------------------
+
+CREATE TABLE secr_plantillas_sesion_extr
+(
+  id integer NOT NULL,
+  valor character varying(10),
+  sustituto character varying(500),
+  vigente numeric(1,0),
+  orden numeric(10,0),
+  CONSTRAINT pk_1980985416 PRIMARY KEY (id)
+);
+ 
+CREATE SEQUENCE spac_sq_1980985416
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+--------------------------------------------------------------------------------
+
+CREATE TABLE secr_plantillas_sesion_exur
+(
+  id integer NOT NULL,
+  valor character varying(10),
+  sustituto character varying(500),
+  vigente numeric(1,0),
+  orden numeric(10,0),
+  CONSTRAINT pk_1980985385 PRIMARY KEY (id)
+);
+ 
+CREATE SEQUENCE spac_sq_1980985385
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+--------------------------------------------------------------------------------
+
+CREATE TABLE secr_plantillas_sesion_ord
+(
+  id integer NOT NULL,
+  valor character varying(10),
+  sustituto character varying(500),
+  vigente numeric(1,0),
+  orden numeric(10,0),
+  CONSTRAINT pk_63893350 PRIMARY KEY (id)
+);
+ 
+CREATE SEQUENCE spac_sq_63893350
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ 
