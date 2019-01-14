0bd7464d79ee0e058ee369ad35d4ce2a07c76365
Manuel Quesada Elvira <manuel_quesada@dipucr.es>
2019-01-14 08:55:04 +0100

    [Dipucr-Manu] Se añade el script de la mejora de la UPNA para que el buscador avanzado filtre los procedimientos por el usuario conectado.

---
+++ sigem/SIGEM_BD/Oracle/actualizacionesQuijote5.0/tramitador/03-ActualizaFrmBusqueda.sql
@@ -0,0 +1,18 @@
+update spac_ct_frmbusqueda set frm_bsq = replace(frm_bsq, 
+'AND TIPO = 1',
+'AND TIPO = 1
+	AND (
+		(
+			USER_UID_PARAM in (select uid_usr from SPAC_SS_FUNCIONES where uid_usr = USER_UID_PARAM and funcion in (3, 4))
+		)
+		or (
+			id in
+			(select sp.id_obj from spac_permisos sp where sp.tp_obj = 1 and
+				(sp.id_resp = USER_UID_PARAM or
+				sp.id_resp = USER_DEPT_UID_PARAM or
+				sp.id_resp in (USER_GROUPS_UID_PARAM)
+				)
+			)
+		)
+ 	)'
+) where descripcion = 'BÚSQUEDA';
