83bdc7bb9d4d0ca6ea8bc034a26f927279a3ad3d
Manuel Quesada Elvira <manuel_quesada@dipucr.es>
2019-02-20 10:17:55 +0100

    [Dipucr-Manu] * Se añaden los scripts de actualización de sgm_adm_entidades

---
+++ sigem/SIGEM_BD/PostgreSQL/actualizacionesQuijote5.0/sigemAdmin/00-Scripts-sgm_adm_entidades.sql
@@ -0,0 +1,5 @@
+alter table sgm_adm_entidades add column password_entidad varchar(50);
+alter table sgm_adm_entidades add column cif varchar(12);
+alter table sgm_adm_entidades add column dir3 varchar(12);
+alter table sgm_adm_entidades add column sia varchar(12);
+alter table sgm_adm_entidades add column deh varchar(12);
