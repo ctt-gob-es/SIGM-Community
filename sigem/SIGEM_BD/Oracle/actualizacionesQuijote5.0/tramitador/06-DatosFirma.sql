ed69f6a766052c93200dc3c4ccc3aef2fb16e84b
Josemi <josemi.bobadilla@unavarra.es>
2019-06-07 12:06:10 +0200

    UPNA-004 - Guardar datos de firma en Base de Datos. Adaptación a sintáxis de Oracle

--- sigem/SIGEM_BD/Oracle/actualizacionesQuijote5.0/tramitador/06-DatosFirma.sql
+++ sigem/SIGEM_BD/Oracle/actualizacionesQuijote5.0/tramitador/06-DatosFirma.sql
@@ -4,31 +4,26 @@ CREATE TABLE spac_datos_firma
 (
   id integer NOT NULL,
   id_documento integer NOT NULL,
-  usuario character varying(32) NOT NULL,
-  nif character varying(20) NOT NULL,
-  nombre character varying(250) NOT NULL,
-  cargo character varying(250),
-  fecha_firma timestamp without time zone NOT NULL,
+  usuario varchar2(32 char) NOT NULL,
+  nif varchar2(20 char) NOT NULL,
+  nombre varchar2(250 char) NOT NULL,
+  cargo varchar2(250 char),
+  fecha_firma timestamp NOT NULL,
   id_circuito integer,
   id_paso integer,
   CONSTRAINT pk_datos_firma PRIMARY KEY (id)
 )
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE spac_datos_firma
-  OWNER TO postgres;
+;
 
 -- SECUENCIA
 CREATE SEQUENCE spac_sq_id_datosfirma
-  INCREMENT 1
+  INCREMENT BY 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-ALTER TABLE spac_sq_id_datosfirma
-  OWNER TO postgres;
+  START WITH 1;  
+  
 
 -- SPAC_CT_ENTIDADES
 INSERT INTO SPAC_CT_ENTIDADES VALUES
 (20, 0, 'SPAC_DATOS_FIRMA', 'ID', '', '', null, 'Datos de firma de documentos', 'SPAC_SQ_ID_DATOSFIRMA', '', '', null);
+
