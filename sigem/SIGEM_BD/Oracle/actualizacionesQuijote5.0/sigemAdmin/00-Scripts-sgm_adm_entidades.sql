a58b396669ae67c71e60ff8ff78633183801eaad
Josemi <josemi.bobadilla@unavarra.es>
2019-11-20 11:25:20 +0100

    UPNA-013 - Actualización de scripts de BBDD de sgm_adm_entidades

--- sigem/SIGEM_BD/Oracle/actualizacionesQuijote5.0/sigemAdmin/00-Scripts-sgm_adm_entidades.sql
+++ sigem/SIGEM_BD/Oracle/actualizacionesQuijote5.0/sigemAdmin/00-Scripts-sgm_adm_entidades.sql
@@ -3,3 +3,8 @@ alter table sgm_adm_entidades add cif varchar2(12 CHAR);
 alter table sgm_adm_entidades add dir3 varchar2(12 CHAR);
 alter table sgm_adm_entidades add sia varchar2(12 CHAR);
 alter table sgm_adm_entidades add deh varchar2(12 CHAR);
+
+-- Actualizar los campos CIF, DIR3, SIA y DEH con los valores propios del organismo de cada entidad.
+-- Por ejemplo, el campo DIR3 se utiliza para la creación del metadato identificador de documento.
+update sgm_adm_entidades
+set cif = 'xxx', dir3 = 'xxx';
