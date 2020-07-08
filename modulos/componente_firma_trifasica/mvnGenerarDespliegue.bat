7a2275cf58fd8f0f042948ec33234acec6eb144a
Agustín Jiménez Moreno <agustin_jimenez@dipucr.es>
2020-07-08 19:31:12 +0200

    [DipuCR-Agustin] Quitar url de repositorio UJI en pom del servidor de firma
    Quitar url de repositorio UJI en pom del servidor de firma

--- modulos/componente_firma_trifasica/mvnGenerarDespliegue.bat
+++ modulos/componente_firma_trifasica/mvnGenerarDespliegue.bat
@@ -1,4 +1,4 @@
-call mvn clean install -Dmaven.test.skip=true
-call mvn clean install -Dmaven.test.skip=true -Dwars
+call mvn clean install -Dmaven.test.skip=true -Dhttps.protocols=TLSv1.2
+#call mvn clean install -Dmaven.test.skip=true -Dwars -Dhttps.protocols=TLSv1.2
 
 pause
